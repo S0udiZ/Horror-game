ed23bbd1348d66e543b6d3b3b26baf59d6c49861
S0udiZ <lukas.dinven@gmail.com>
2025-11-06 09:21:43 +0100

    Balls updated

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,16 +4,24 @@ public class Ball : MonoBehaviour
 {
     Rigidbody rb;
     public float MaxForce;
+    public bool reset = false;
+    public Transform transform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(Random.Range(0, MaxForce),0,0));
+        rb.AddForce(new Vector3(Random.Range(-MaxForce, MaxForce), 0, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (reset)
+        {
+            transform.position = new Vector3(0, 4.5f, 1.5f);
+            rb.linearVelocity = new Vector3(0, 0, 0);
+            reset = false;
+            Start();
+        }
     }
 }
