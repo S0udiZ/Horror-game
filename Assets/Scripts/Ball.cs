1f01478d86605528f4929fec28dc37dfd310f019
1rasmusrune1 <1rasmusrune1@gmail.com>
2025-11-13 09:25:42 +0100

    Updated plinko code
    Updated the plinko code, adding various features like random start torque, automatic reset when hitting the bottom, and reset if it stalls.
    I also renamed the ball physics material.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,23 +4,52 @@ public class Ball : MonoBehaviour
 {
     Rigidbody rb;
     public float MaxForce;
-    public bool reset = false;
+	public float MaxTorque;
+	public Vector3 ForceBase;
+	public Vector3 TorqueBase;
+    public int maxStall;
+    public float endY;
+	public bool DEBUG_RESET = false;
+
+    Vector3 spawn;
+    float minY;
+    int stallTime = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(Random.Range(-MaxForce, MaxForce), 0, 0));
+		spawn = transform.position;
+        Drop();
     }
 
+    void Drop() {
+		rb.AddForce(ForceBase*Random.Range(-MaxForce, MaxForce), ForceMode.VelocityChange);
+		rb.AddTorque(TorqueBase*Random.Range(-MaxTorque, MaxTorque), ForceMode.VelocityChange);
+        minY = transform.position.y;
+        stallTime = 0;
+	}
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (reset)
+        if (transform.position.y <= endY) {
+			DEBUG_RESET = true; //TODO handle ball stall.
+		} else if (transform.position.y < minY) {
+            minY = transform.position.y;
+            stallTime = 0;
+        } else {
+            stallTime++;
+            if (stallTime >= maxStall) {
+                DEBUG_RESET = true; //TODO handle ball stall.
+            }
+        }
+        if (DEBUG_RESET)
         {
-            transform.position = new Vector3(0, 4.5f, 1.5f);
+            transform.position = spawn;
             rb.linearVelocity = new Vector3(0, 0, 0);
-            reset = false;
-            Start();
+			DEBUG_RESET = false;
+            Drop();
         }
     }
 }
